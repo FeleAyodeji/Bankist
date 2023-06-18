647bd58a80833405b5bba93c724343546ee06bbe
Fele Ayodeji <feledeji@gmail.com>
2023-06-18 22:40:47 +0100

    movement iteration

--- script.js
+++ script.js
@@ -73,4 +73,22 @@ const currencies = new Map([
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
+// using for IN loop
+for (const [i, movement] of movements.entries) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+//using forEach loop
+movements.forEach(function (movement, index, array) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+});
+
 /////////////////////////////////////////////////
